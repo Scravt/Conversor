a0e621ceb10cfc875d16b9ea155872465c197383
Gonzalo <34680110ax@gmail.com>
2023-05-14 21:55:24 -0300

    first commit

---
+++ src/main/java/com/mycompany/conversor/Conversor.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conversor;
+
+/**
+ *
+ * @author el_go
+ */
+public class Conversor {
+
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        ventana.show();
+    }
+}
